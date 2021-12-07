53be25fa9926f9e6effc78da93c07e8e1e1e2998
Rhaegar2012 <48417377+Rhaegar2012@users.noreply.github.com>
2021-12-06 21:49:25 -0800

    12062021-Update
    Website Developed

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function HelloWorld(){
-    alert("hello world");
+//Global variable for flow input list 
+let flowInputList=[];
+//Generate a table of cashflows based on the  number of periods inmputted by the user
+function generateCashFlows(){
+    let numberOfTerms= parseInt(document.getElementById("termInput").value);
+    let tableRow=""
+    let term=1;
+    for(let i=0; i<numberOfTerms;i++){
+        let inputId=`input${term}`
+        flowInputList.push(inputId);
+        tableRow+=`<tr><td>Term ${term}</td><td><input type="number" id="${inputId}" class="form-control"></td>`;
+        term+=1;
+    }
+    document.getElementById("flowTable").innerHTML=tableRow;
+}
+function calculateCouponBondValuation(){
+   let couponSum=0;
+   let couponValuation=0;
+   let discountRateInput=parseFloat(document.getElementById("discountRateInput").value);
+   let numberOfTerms= parseInt(document.getElementById("termInput").value);
+   for(let i=0;i<flowInputList.length;i++){
+       let inputElement=flowInputList[i]
+       let coupon=parseFloat(document.getElementById(inputElement).value);
+       couponSum+=coupon;
+   }
+   couponValuation=couponSum/((1+discountRateInput)**(numberOfTerms));
+   document.getElementById("couponValidationResult").innerHTML=couponValuation;
+
+}
+function calculateFaceValuation(){
+    let faceValue=parseFloat(document.getElementById("faceValueInput").value);
+    let discountRateInput=parseFloat(document.getElementById("discountRateInput").value);
+    let timeToMaturity=parseFloat(document.getElementById("maturityInput").value);
+    let presentValue=faceValue/((1+discountRateInput)**timeToMaturity);
+    document.getElementById("faceValueResult").innerHTML=presentValue;
+
 }
